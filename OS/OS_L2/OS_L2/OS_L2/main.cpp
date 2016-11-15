c3a59485d63a0642217ac2310cd7eb7d2624bccd
sandyre <aleksanteri@niuitmo.ru>
2016-11-15 03:09:00 +0300

    added OS lab2 (var 2), code cleaning done in prev rev

--- OS/OS_L2/OS_L2/OS_L2/main.cpp
+++ OS/OS_L2/OS_L2/OS_L2/main.cpp
@@ -11,8 +11,7 @@
 #include <chrono>
 #include <queue>
 
-#define PROCESS_TACT 1
-float gCurrentTime = 0.0f;
+float gTimer = 0.0f;
 
 class Process
 {
@@ -23,23 +22,27 @@ public:
         FINISHED = 0x2
     };
     
-    Process() : m_fRemainingRunningTime(0.0f),
-                m_eState(RUNNING)
+    Process() : m_eState(RUNNING),
+                m_fProcessStartingTime(0.0f),
+                m_fRemainingRunningTime(0.0f)
     {
     }
     
-    void  MakeStep()
+    void MakeStep()
     {
-        if( m_fRemainingRunningTime - 1.0f == 0 )
+        if(m_eState == RUNNING)
         {
-            std::this_thread::sleep_for(std::chrono::milliseconds(PROCESS_TACT));
-            m_fRemainingRunningTime -= 1.0f;
-            m_eState = FINISHED;
-        }
-        else
-        {
-            m_fRemainingRunningTime -= 1.0f;
-            std::this_thread::sleep_for(std::chrono::milliseconds(PROCESS_TACT));
+            if(m_fRemainingRunningTime - 1.0f == 0)
+            {
+                m_fRemainingRunningTime -= 1.0f;
+                m_eState = FINISHED;
+                std::cout << "Process finished. It took " << gTimer - m_fProcessStartingTime;
+                std::cout << std::endl;
+            }
+            else
+            {
+                m_fRemainingRunningTime -= 1.0f;
+            }
         }
     }
     
@@ -47,7 +50,7 @@ public:
     {
         return m_eState;
     }
-    
+
     float GetRemainingRunningTime() const
     {
         return m_fRemainingRunningTime;
@@ -56,162 +59,116 @@ public:
     {
         m_fRemainingRunningTime = fValue;
     }
+    void SetProcessStartingTime(float fValue)
+    {
+        m_fProcessStartingTime = fValue;
+    }
     
 protected:
     enum ProcessState m_eState;
+    float m_fProcessStartingTime;
     float m_fRemainingRunningTime;
 };
 
-enum ProcessManagerState
-{
-    RUNNING = 0x1,
-    FINISHED = 0x2
-};
-
-class RR_Manager
+class RR_Algorithm
 {
 public:
-    RR_Manager(float fQuant) : m_fQuant(fQuant),
-                               m_fTimer(0.0f),
-                               m_pCurrentRunningProcess(nullptr)
+    RR_Algorithm(const float fQuant) : m_fQuant(fQuant),
+                                       m_iCurrentlyRunningProcess(0)
     {
-        
     }
     
-    void    MakeStep()
+    void MakeStep()
     {
         if(m_queProcesses.size() == 0)
-        {
-            m_eState = FINISHED;
-        }
+            return;
         
-        while(m_fTimer <= m_fQuant &&
-              m_queProcesses[m_CurrentRunningProcessIndex]->GetProcessState() != Process::FINISHED)
+        for(float i = 0.0f; i < m_fQuant; ++i)
         {
-            m_queProcesses[m_CurrentRunningProcessIndex]->MakeStep();
-            m_fTimer += 1.0f;
-        }
-        
-        if(m_queProcesses[m_CurrentRunningProcessIndex]->GetProcessState() == Process::FINISHED)
-        {
-            m_queProcesses.erase(m_queProcesses.begin() +
-                                 m_CurrentRunningProcessIndex);
-        }
-        else
-        {
-            if(m_CurrentRunningProcessIndex == m_queProcesses.size()-1)
+            m_queProcesses[m_iCurrentlyRunningProcess].MakeStep();
+            if(m_queProcesses[m_iCurrentlyRunningProcess].GetProcessState() ==
+               Process::FINISHED)
             {
-                m_CurrentRunningProcessIndex = 0;
-            }
-        }
-    }
-    
-    void    AddProcess(Process * pProcess)
-    {
-        if(m_queProcesses.size() == 0)
-        {
-            m_CurrentRunningProcessIndex = 0;
-        }
-        
-        m_eState = RUNNING;
-        m_queProcesses.push_back(pProcess);
-    }
-    
-    ProcessManagerState GetState() const
-    {
-        return m_eState;
-    }
-    
-protected:
-    enum ProcessManagerState m_eState;
-    const float              m_fQuant;
-    float                    m_fTimer;
-    size_t                   m_CurrentRunningProcessIndex;
-    Process *                m_pCurrentRunningProcess;
-    std::deque<Process*>     m_queProcesses;
-};
-
-class SPN_ProcessManager
-{
-public:
-    SPN_ProcessManager() : m_pCurrentRunningProcess(nullptr)
-    {
-        
-    }
-    
-    void    MakeStep()
-    {
-        if(m_pCurrentRunningProcess == nullptr)
-        {
-            m_pCurrentRunningProcess = m_queProcesses.front();
-            if(m_pCurrentRunningProcess != nullptr)
-            {
-                for(auto& proc : m_queProcesses)
-                {
-                    if(proc->GetRemainingRunningTime() <
-                       m_pCurrentRunningProcess->GetRemainingRunningTime())
-                    {
-                        m_pCurrentRunningProcess = proc;
-                    }
-                }
-            }
-        }
-        
-        if(m_pCurrentRunningProcess != nullptr)
-        {
-            m_pCurrentRunningProcess->MakeStep();
-        }
-        
-        if(m_pCurrentRunningProcess->GetProcessState() == Process::FINISHED)
-        {
-            for(auto procIter = m_queProcesses.begin();
-                procIter != m_queProcesses.end();
-                ++procIter)
-            {
-                Process * pProc = *procIter;
-                if(pProc->GetProcessState() == Process::FINISHED)
-                {
-                    m_queProcesses.erase(procIter);
-                    break;
-                }
-            }
-            
-            m_pCurrentRunningProcess = nullptr;
-        }
-    }
-    
-    void    AddProcess(Process * pProcess)
-    {
-        if(m_pCurrentRunningProcess != nullptr)
-        {
-            if(m_pCurrentRunningProcess->GetRemainingRunningTime() >
-               pProcess->GetRemainingRunningTime())
-            {
-                m_queProcesses.push_back(pProcess);
-                m_pCurrentRunningProcess = m_queProcesses.back();
+                m_queProcesses.erase(m_queProcesses.begin() + m_iCurrentlyRunningProcess);
+                if(m_iCurrentlyRunningProcess == m_queProcesses.size())
+                    m_iCurrentlyRunningProcess = 0;
+                else
+                    ++m_iCurrentlyRunningProcess;
                 return;
             }
         }
-        
-            // Stands for first process adding
-        m_queProcesses.push_back(pProcess);
-        m_pCurrentRunningProcess = m_queProcesses.front();
     }
     
-    ProcessManagerState GetState() const
+    void AddProcess(Process oProc)
     {
-        return m_eState;
+        if(m_iCurrentlyRunningProcess == 0)
+        {
+            m_queProcesses.push_back(oProc);
+        }
+        else
+        {
+            m_queProcesses.insert(m_queProcesses.begin() + m_iCurrentlyRunningProcess - 1,
+                                  oProc);
+        }
     }
+    
+    size_t GetProcessesCount() const
+    {
+        return m_queProcesses.size();
+    }
+    
 protected:
-    Process *                m_pCurrentRunningProcess;
-    enum ProcessManagerState m_eState;
-    std::deque<Process*> m_queProcesses;
+    size_t m_iCurrentlyRunningProcess;
+    std::deque< Process > m_queProcesses;
+    const float m_fQuant;
+};
+
+class SPN_Algorithm
+{
+public:
+    SPN_Algorithm()
+    {
+    }
+    
+    void MakeStep()
+    {
+        if(m_queProcesses.size() == 0)
+            return;
+        
+        m_queProcesses.front().MakeStep();
+        if(m_queProcesses.front().GetProcessState() ==
+           Process::FINISHED)
+        {
+            m_queProcesses.pop_front();
+            return;
+        }
+    }
+    
+    void AddProcess(Process oProc)
+    {
+        size_t iInsertionIndex = 0;
+        for(auto i = 0; i < m_queProcesses.size(); ++i)
+        {
+            if(m_queProcesses[i].GetRemainingRunningTime() <= oProc.GetRemainingRunningTime())
+                ++iInsertionIndex;
+        }
+        m_queProcesses.insert(m_queProcesses.begin() + iInsertionIndex,
+                              oProc);
+    }
+    
+    size_t GetProcessesCount() const
+    {
+        return m_queProcesses.size();
+    }
+    
+protected:
+    std::deque< Process > m_queProcesses;
 };
 
 int main(int argc, const char * argv[])
 {
-    SPN_ProcessManager oProcessManager;
-    RR_Manager oRRManager(4.0f);
+    SPN_Algorithm oSPNManager;
+    RR_Algorithm oRRManager(4.0f);
     
     std::queue<std::pair<float, float>> queProcesses;
     queProcesses.push(std::make_pair(0.0f, 4.0f));
@@ -223,27 +180,34 @@ int main(int argc, const char * argv[])
     queProcesses.push(std::make_pair(19.0f, 2.0f));
     queProcesses.push(std::make_pair(24.0f, 5.0f));
     
-    while(queProcesses.size() != 0 &&
-          oProcessManager.GetState() != ProcessManagerState::FINISHED)
+    while(true)
     {
         if(queProcesses.size() != 0)
         {
-            if(gCurrentTime == queProcesses.front().first)
+            if(gTimer == queProcesses.front().first)
             {
-                Process * newProcess = new Process;
-                newProcess->SetRemainingRunningTime(queProcesses.front().second);
-//                oProcessManager.AddProcess(newProcess);
-                oRRManager.AddProcess(newProcess);
+                Process oProc;
+                oProc.SetProcessStartingTime(queProcesses.front().first);
+                oProc.SetRemainingRunningTime(queProcesses.front().second);
+                oRRManager.AddProcess(oProc);
+                oSPNManager.AddProcess(oProc);
                 
                 queProcesses.pop();
             }
         }
         
-        oRRManager.MakeStep();
-//        oProcessManager.MakeStep();
+        gTimer += 1.0f;
         
-        gCurrentTime += 1.0f;
+        oRRManager.MakeStep();
+        oSPNManager.MakeStep();
+        
+        if(queProcesses.size() == 0 &&
+           oRRManager.GetProcessesCount() == 0 &&
+           oSPNManager.GetProcessesCount() == 0)
+            break;
     }
     
-    std::cout << "Done in " << gCurrentTime;
+    std::cout << "Done in " << gTimer << std::endl;
+    
+    return 0;
 }
